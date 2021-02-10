d02f9a6a89178e1ec89259d1cec6c6aa9d2393b1
vikasrahar <78865397+vikasrahar@users.noreply.github.com>
2021-02-10 22:29:01 +0530

    Delete Data Analysis directory

--- Data Analysis/Assignment -2/BDS3.R
+++
@@ -1,7 +0,0 @@
-x = seq(100,1500,400)
-y = rep(0,4)
-for(num in 1:4){
-  y[num] = BDScalculate(0.01,x[num])
-  print(y[num])
-}
-plot(x,y, main ="Graph respresentation", xlab="B",ylab="TEST MSE", type="b")
